c8d94ef31519885f0c279cc0809d0f4668ad3a18
developpeur fou <moi@ici_ou_la.everywhere>
2015-02-10 11:52:49 +0100

    initial my-java-app1

---
+++ my-java-app1/src/main/java/com/mycompany/app1/Calculateur.java
@@ -0,0 +1,17 @@
+package com.mycompany.app1;
+
+/**
+ * Calculateur
+ *
+ */
+public class Calculateur 
+{
+    public double add(double a,double b){
+      return a+b;
+    }
+    
+    public double mult(double a,double b){
+      return a*b;
+    }
+    
+}
